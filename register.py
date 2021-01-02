bac03cfdfd396fe2ef6c186a5638e175292c5bb3
Germey <cqc@cuiqingcai.com>
2021-01-03 02:15:04 +0800

    update account

--- register.py
+++ register.py
@@ -3,6 +3,8 @@ from acinonyx import run
 import requests
 from loguru import logger
 
+# This is a script for registering account for antispider7, using acinonyx to accelerate.
+
 parser = argparse.ArgumentParser(description='AccountPool')
 parser.add_argument('website', type=str, help='website')
 args = parser.parse_args()
@@ -12,16 +14,15 @@ website = args.website
 @logger.catch()
 def register(username, password):
     logger.debug(f'register using {username} and {password}')
-    response = requests.post('https://antispider7.scrape.center/api/register', json={
+    response = requests.post(f'https://{website}.scrape.center/api/register', json={
         'username': username,
         'password': password
     })
     print(response.json())
 
 
-accounts = []
-for index in range(1, 1000):
-    accounts.append((f'admin{index}', f'admin{index}'))
-print(accounts)
-
-run(register, accounts)
+if __name__ == '__main__':
+    accounts = []
+    for index in range(1, 1000):
+        accounts.append((f'admin{index}', f'admin{index}'))
+    run(register, accounts)
